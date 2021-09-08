0e798da87071b94bedadc813330a3d519aebcab9
Daniel Alvsåker <daniel@timeanddate.com>
2021-09-08 10:10:25 +0200

    Deserialize an empty string to None

--- libtad-models/src/places/geo.rs
+++ libtad-models/src/places/geo.rs
@@ -14,8 +14,46 @@ pub struct Geo {
     pub country: Country,
 
     /// Geographical latitude of the location.
+    #[serde(deserialize_with = "custom_deserialize::float_or_empty_string", default)]
     pub latitude: Option<f32>,
 
     /// Geographical longitude of the location.
+    #[serde(deserialize_with = "custom_deserialize::float_or_empty_string", default)]
     pub longitude: Option<f32>,
+}
+
+
+mod custom_deserialize {
+    use serde::de::{self, value, Deserialize, Deserializer, SeqAccess, Visitor};
+
+
+    pub fn float_or_empty_string<'de, D>(deserializer: D) -> Result<Option<f32>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct FloatOrString;
+
+        impl<'de> Visitor<'de> for FloatOrString {
+            type Value = Option<f32>;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("float or empty string")
+            }
+
+            fn visit_str<E>(self, _: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(None)
+            }
+
+            fn visit_f64<E>(self, s: f64) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(Some(s as f32))
+            }
+        }
+        deserializer.deserialize_any(FloatOrString)
+    }
 }
